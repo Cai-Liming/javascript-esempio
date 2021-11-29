4258cc679a55cfd596d36d348bbe9a8faf6f0bac
Cai-Liming <cai.liming@iisgalvanimi.edu.it>
2021-11-29 12:59:06 +0000

    fattoria

---
+++ script.js
@@ -0,0 +1,28 @@
+function button1(){
+
+    const animali={
+    tipo:"mucca",
+    anni:"5",
+    sesso:"maschio",
+    inteligezza: "2"
+    }
+    document.getElementById("demo").innerHTML = "il tipo : " +" "+ animali.tipo +" "+ "gli anni sono :"+animali.anni + " " +"il sesso :"+ animali.sesso +" "+ "inteligenza :" + animali.inteligezza
+}
+function button2(){
+    const animali1={
+        tipo:"cane",
+        anni:"6",
+        sesso:"maschio",
+        inteligenza : "6"
+    }
+    document.getElementById("demo").innerHTML = "il tipo : " +" "+ animali1.tipo +" "+ "gli anni sono :"+animali1.anni + " " +"il sesso :"+ animali1.sesso +" "+ "inteligenza :" + animali1.inteligezza
+}
+function button3(){
+    const animali2={
+        tipo:"gatta",
+        anni:"3",
+        sesso:"femmina",
+        inteligenza : "5"
+    }
+    document.getElementById("demo").innerHTML = "il tipo : " +" "+ animali2.tipo +" "+ "gli anni sono :"+animali2.anni + " " +"il sesso :"+ animali2.sesso +" "+ "inteligenza :" + animali2.inteligezza
+}
